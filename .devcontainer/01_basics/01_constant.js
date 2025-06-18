23b702ef5677df42588849e8c7b46c8d99ad903b
Sejuu11 <sejalkale9322@gmail.com>
2025-06-18 13:44:50 +0000

    datatypes

---
+++ .devcontainer/01_basics/01_constant.js
@@ -0,0 +1,14 @@
+const accountId= 123445
+let accountEmail="sejal@gmail.com"
+var accountPassword="2345"
+accountCity="pune"
+accountEmail="hc@gmail.com"
+accountPassword="123"
+accountCity="mumbai"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+/*
+prefer not to use var 
+because issues in block scope and function scope
+*/
---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,16 @@
+let name ="Ajya"
+let isloggedIn = false;
+let state;
+
+// number
+// bigint
+// string=""
+// boolean=false/true
+//null= standalon value (empty)
+//undefined=value is not assigned
+//symbol=to find uniqueness
+//object
+
+console.log(typeof "ajya");
+console.log(typeof age);
+console.log(typeof null);//object
