714d2fc9623e673f1d91c08626a2861254d74e3b
Sejuu11 <sejalkale9322@gmail.com>
2025-06-19 14:01:24 +0000

    comparison of datatypes

---
+++ .devcontainer/01_basics/04_comparison.js
@@ -0,0 +1,7 @@
+console.log(null>0);
+console.log(null == 0);
+console.log( null>=0); //in javascript null is converted as a zero 
+ 
+/// strictCheck is like ===
+
+console.log("2"===2);// differet types of datatypes
